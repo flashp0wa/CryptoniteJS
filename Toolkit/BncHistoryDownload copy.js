7745eef3047bb88ec9567b6a034128b5c6f8a6bc
David <77695601+flashp0wa@users.noreply.github.com>
2025-10-15 12:01:19 -0700

    Create BncHistoryDownload copy.js

---
+++ Toolkit/BncHistoryDownload copy.js
@@ -0,0 +1,283 @@
+const _ = require('lodash');
+const util = require('util');
+const fs = require('fs');
+const extract = require('extract-zip');
+const download = require('download');
+const {getDatesArray} = require('../Toolkit/OnDateOperations.js');
+const {BncHistoryDownloadLog} = require('./Logger.js');
+const {getExchanges} = require('../Classes/Exchanges/ExchangesClass');
+const {getDatabase} = require('../Classes/Database.js');
+
+const db = getDatabase();
+const downloadPath = `${process.env.CRYPTONITE_ROOT}\\Inboxes\\BinanceData`;
+
+async function downloadHistoryData(inObj) {
+  return new Promise(async (resolve) => {
+    const baseUrl = 'https://data.binance.vision/data/spot';
+    const alreadyDownloaded = [];
+    let oneSuccessfulDownload = false;
+    let alreadyDownloadedFile = false;
+    let fileName;
+    let dates;
+    let url;
+
+
+    const datesArr = [];
+    const yesterday = new Date().setDate(new Date().getDate() -1);
+    let startDate;
+    let endDate;
+    if (typeof inObj.startDate === 'string') {
+      startDate = new Date(inObj.startDate);
+      endDate = inObj.endDate ? new Date(inObj.endDate) : false;
+    }
+
+    if (yesterday < endDate) {
+      BncHistoryDownloadLog.log({
+        level: 'error',
+        message: 'End Date cannot be bigger than yesterdays date',
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+      throw (new Error('End Date cannot be bigger than yesterdays date'));
+    }
+    if (!endDate) {
+      if (inObj.timeFrame === 'daily') {
+        dates = [startDate.toISOString().split('T')[0]];
+      } else {
+        dates = [startDate.toISOString().split('T')[0].slice(0, 7)];
+      }
+    } else {
+      if (inObj.timeFrame === 'daily') {
+        tempArray = getDatesArray(startDate, endDate, 'days');
+        datesArr.push(tempArray);
+      } else {
+        tempArray = getDatesArray(startDate, endDate, 'months');
+        datesArr.push(tempArray);
+      }
+      dates = _.flatten(datesArr);
+    }
+
+    fs.readdirSync(downloadPath).forEach((file) => {
+      alreadyDownloaded.push(file);
+    });
+
+
+    for (const date of dates) {
+      if (inObj.tradeType === 'klines') {
+        fileName = `${inObj.symbol}-${inObj.klinesTimeFrame}-${date}.zip`;
+        url = `${baseUrl}/${inObj.timeFrame}/${inObj.tradeType}/${inObj.symbol}/${inObj.klinesTimeFrame}/${fileName}`;
+      } else {
+        fileName = `${inObj.symbol}-${inObj.tradeType}-${date}.zip`;
+        url = `${baseUrl}/${inObj.timeFrame}/${inObj.tradeType}/${inObj.symbol}/${fileName}`;
+      }
+
+      if (alreadyDownloaded.length > 0) {
+        for (const file of alreadyDownloaded) {
+          if (file === fileName) {
+            alreadyDownloadedFile = true;
+            break;
+          }
+        }
+
+        if (alreadyDownloadedFile) {
+          BncHistoryDownloadLog.log({
+            level: 'info',
+            message: `${fileName} already downloaded`,
+            senderFunction: 'downloadHistoryData',
+            file: 'BncHistoryDownload.js',
+          });
+          alreadyDownloadedFile = false;
+          continue;
+        }
+      }
+
+
+      try {
+        BncHistoryDownloadLog.log({
+          level: 'info',
+          message: `Downloading from URL: ${url} to ${downloadPath}`,
+          senderFunction: 'downloadHistoryData',
+          file: 'BncHistoryDownload.js',
+        });
+        BncHistoryDownloadLog.log({
+          level: 'info',
+          message: `Downloading: ${fileName}`,
+          senderFunction: 'downloadHistoryData',
+          file: 'BncHistoryDownload.js',
+        });
+        await download(url, downloadPath);
+        oneSuccessfulDownload = true;
+        BncHistoryDownloadLog.log({
+          level: 'info',
+          message: 'Download completed',
+          senderFunction: 'downloadHistoryData',
+          file: 'BncHistoryDownload.js',
+        });
+      } catch (error) {
+        if (util.inspect(error.message).includes('404')) {
+          BncHistoryDownloadLog.log({
+            level: 'info',
+            message: 'Date does not exist',
+            senderFunction: 'downloadHistoryData',
+            file: 'BncHistoryDownload.js',
+          });
+          if (oneSuccessfulDownload) {
+            BncHistoryDownloadLog.log({
+              level: 'info',
+              message: 'Symbol discontinued',
+              senderFunction: 'downloadHistoryData',
+              file: 'BncHistoryDownload.js',
+            });
+            resolve(true);
+            return;
+          }
+          continue;
+        }
+        BncHistoryDownloadLog.log({
+          level: 'error',
+          message: `Error while downloading. ${error}`,
+          senderFunction: 'downloadHistoryData',
+          file: 'BncHistoryDownload.js',
+        });
+        continue;
+      }
+    }
+    resolve(true);
+  });
+}
+
+async function binanceHistoryData(inObj) {
+  let counter = 1;
+  const klinesArr = [
+    '1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1mo',
+  ];
+  const symbols = [...getExchanges()['binance'].symbolList];
+
+  if (inObj.symbol === 'all' && inObj.klinesTimeFrame === 'all') {
+    for (const symbol of symbols) {
+      for (const kline of klinesArr) {
+        counter++;
+        BncHistoryDownloadLog.log({
+          level: 'info',
+          message: `Current: ${counter} / ${symbols.length * klinesArr.length}`,
+          senderFunction: 'downloadHistoryData',
+          file: 'BncHistoryDownload.js',
+        });
+        inObj.symbol = symbol;
+        inObj.klinesTimeFrame = kline;
+        await downloadHistoryData(inObj);
+      }
+    }
+  } else if (inObj.symbol === 'all') {
+    for (const symbol of symbols) {
+      counter++;
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `Current: ${counter} / ${symbols.length}`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+      inObj.symbol = symbol;
+      await downloadHistoryData(inObj);
+    }
+  } else if (inObj.symbol === 'allUSDT') {
+    const usdtSymbols = [];
+
+    for (const symbol of symbols) {
+      if (symbol.match(/.*USDT/)) {
+        usdtSymbols.push(symbol);
+      }
+    }
+
+    for (const symbol of usdtSymbols) {
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `Current: ${counter} / ${usdtSymbols.length}`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+      counter++;
+      inObj.symbol = symbol;
+      await downloadHistoryData(inObj);
+    }
+  } else if (inObj.klinesTimeFrame === 'all') {
+    for (const kline of klinesArr) {
+      counter++;
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `Current: ${counter} / ${klinesArr.length}`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+      inObj.klinesTimeFrame = kline;
+      await downloadHistoryData(inObj);
+    }
+  } else {
+    await downloadHistoryData(inObj);
+  }
+
+  BncHistoryDownloadLog.log({
+    level: 'info',
+    message: 'Processing downloaded files...',
+    senderFunction: 'downloadHistoryData',
+    file: 'BncHistoryDownload.js',
+  });
+
+  const zipFiles = fs.readdirSync(downloadPath);
+
+  for (const file of zipFiles) {
+    const filePathFull = `${downloadPath}\\${file}`;
+    const symbol = file.split('-')[0];
+    const timeFrame = file.split('-')[1];
+    const csvFile = file.split('.')[0] + '.csv';
+    const csvPathFull = downloadPath + '\\' +csvFile;
+    try {
+      await extract(filePathFull, {dir: downloadPath});
+
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `${file} extract done`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+
+      fs.unlinkSync(filePathFull);
+
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `${file} delete done`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+
+      await db.sproc_ImportBinanceCsv(symbol, timeFrame, csvPathFull);
+
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `${csvFile} import done`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+
+      fs.unlinkSync(csvPathFull);
+
+      BncHistoryDownloadLog.log({
+        level: 'info',
+        message: `${csvFile} delete done`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+    } catch (error) {
+      BncHistoryDownloadLog.log({
+        level: 'error',
+        message: `There was an error in processing binance data. ${error}`,
+        senderFunction: 'downloadHistoryData',
+        file: 'BncHistoryDownload.js',
+      });
+    }
+  }
+}
+
+module.exports = {
+  binanceHistoryData,
+};
